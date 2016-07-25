e5560bb1684e3bf39b11ceb0787144aac442594b
Chip Carnes <chip.carnes27@gmail.com>
2016-07-24 19:56:59 -0700

    Prep for merge with spec updates and ux changes

--- spec/brand_spec.rb
+++ spec/brand_spec.rb
@@ -10,4 +10,13 @@ describe(Brand) do
     brand = Brand.create({:name => "brooks"})
     expect(brand.name()).to(eq("Brooks"))
   end
+
+  it('should have many stores') do
+    brand = Brand.create({:name => "brooks"})
+    store = Store.create({name: "John's New Shoes Plus", street: "123 Main Street", city: "Lexington", state: "KY", zip: "40506", phone_number: "555-123-1234", owner: "Franz Schubert"})
+    store2 = Store.create({name: "Fleet Flyers", street: "123 Main Street", city: "Bellvue", state: "KY", zip: "40506", phone_number: "555-123-1234", owner: "John Mike"})
+    brand.stores.push(store)
+    brand.stores.push(store2)
+    expect(brand.stores()).to(eq([store, store2]))
+  end
 end
